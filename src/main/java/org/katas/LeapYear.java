fa543239a4a965b43895fe2d06eb49906a728806
Dalvir Gill <DYG@phoebussoftware.com>
2023-08-24 11:56:48 +0100

    Added first failing test

---
+++ src/main/java/org/katas/LeapYear.java
@@ -0,0 +1,7 @@
+package org.katas;
+
+public class LeapYear {
+    public static boolean IsLeapYear(int i) {
+        return true;
+    }
+}
--- src/test/java/KataTest.java
+++ src/test/java/KataTest.java
@@ -1,6 +1,12 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.katas.LeapYear;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class KataTest {
     @Test
-    public void ShouldFail() { }
+    public void IsLeapYearShouldReturnFalseFor1967() {
+        assertFalse(LeapYear.IsLeapYear(1967));
+    }
 }
